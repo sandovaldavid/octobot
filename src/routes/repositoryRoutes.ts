324dfd0cc74578312defc14a20a7d1a7015f717d
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-18 02:23:24 -0500

    ✨ add updateRepository and deleteRepository routes to repositoryRoutes

--- src/routes/repositoryRoutes.ts
+++ src/routes/repositoryRoutes.ts
@@ -8,5 +8,7 @@ router.get('/repositories/github', repositoryController.getAllRepositories);
 router.post('/repositories/sync', repositoryController.syncRepositories);
 router.get('/repositories/stored', repositoryController.getStoredRepositories);
 router.post('/repositories', repositoryController.createRepository);
+router.patch('/repositories/:repoName', repositoryController.updateRepository);
+router.delete('/repositories/:repoName', repositoryController.deleteRepository);
 
 export default router;
