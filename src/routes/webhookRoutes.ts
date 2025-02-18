835885b18ca8b053366217e0d96387a82472799b
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-18 15:02:59 -0500

    ✨ add route for configuring repository webhooks in webhookRoutes

--- src/routes/webhookRoutes.ts
+++ src/routes/webhookRoutes.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post('/webhooks/github', webhookController.handleWebhook);
 router.post('/webhooks/github/test', webhookController.testWebhook);
+router.post('/webhooks/github/repository/:repoName', webhookController.configureRepositoryWebhook);
 
 export default router;
