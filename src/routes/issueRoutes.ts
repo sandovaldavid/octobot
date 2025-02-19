76c5739d12eae5b508357f62f67b97e1e308713e
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-19 15:39:27 -0500

    ✨ add syncIssues route to issueRoutes for synchronizing issues

--- src/routes/issueRoutes.ts
+++ src/routes/issueRoutes.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/issues', issueController.getIssues);
 router.get('/issues/:issueNumber', issueController.getIssueById);
 router.post('/issues', issueController.createIssue);
+router.post('/sync', issueController.syncIssues);
 
 export default router;
